d940b99ea756d38973bcd28a4babacd9c55df406
Clive <lin.zhp@gmail.com>
2010-02-09 23:43:51 +0800

    add build_demo.bat

---
+++ build_demo.bat
@@ -0,0 +1,1 @@
+mxmlc -compiler.source-path=src -compiler.fonts.managers=flash.fonts.AFEFontManager -compiler.include-libraries=bin/HemlockCore.swc -output=src/com/mintdigital/drawingDemo/containers/DrawingDemoContainer.swf src/com/mintdigital/drawingDemo/containers/DrawingDemoContainer.as
